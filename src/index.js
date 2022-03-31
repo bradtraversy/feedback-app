993f755069305aa86f1e98dec144037560589944
Will Adams <bushblade@gmail.com>
2022-03-31 11:45:55 +0100

    update to React 18

--- src/index.js
+++ src/index.js
@@ -1,11 +1,6 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
